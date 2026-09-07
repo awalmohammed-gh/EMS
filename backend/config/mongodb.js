14031f578ed982f263acf26f0089721d2bbe0216
Awal Mohammed <awalmohammed0013@gmail.com>
2026-09-07 07:08:16 +0000

    changes the fucntion of mongodb config

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,19 +1,14 @@
 import mongoose from "mongoose";
-import dns from "dns"
+import dns from "dns";
 
-dns.setServers(["1.1.1.1","8.8.8.8"])
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 export const connectMongodb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected successfully");
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.log("MongoDB Connection Error:", error.message);
     console.log("MongoDB Connection Error:", error.message);
   }
 };
@@ -25,11 +20,5 @@ export const closeMongodb = async () => {
     console.log("MongoDB connection closed");
   } catch (error) {
     console.log("Error closing MongoDB:", error.message);
-    await mongoose.connection.close();
-
-    console.log("MongoDB connection closed");
-  } catch (error) {
-    console.log("Error closing MongoDB:", error.message);
   }
 };
-
